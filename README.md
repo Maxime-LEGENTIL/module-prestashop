97369f4379cd202b39494a46af3139d2d0af3e33
Maxime LE GENTIL <maxime.legentil17@gmail.com>
2024-06-27 00:57:11 +0200

    Edit readme

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Module pour Prestashop
 
 URL d'accès en requête GET depuis le navigateur pour un retour en JSON :
-https://nomdedomaine+extension/module/nomdumodule/nomdumodule
+https://nomdedomaine+extension/module/nomdumodule/
+
+## Help :
+Architecture :
+nomdumodule
+    controllers
+        front
+            nomdumodule.php
+    nomdumodule.php
+    index.php ()
+    logo.png (détecté automatiquement sur le backoffice)
--- searchproducts/searchproducts.php
+++ searchproducts/searchproducts.php
@@ -10,17 +10,17 @@ class SearchProducts extends Module
         $this->name = 'searchproducts';
         $this->tab = 'front_office_features';
         $this->version = '1.0.0';
-        $this->author = 'Votre Nom';
+        $this->author = 'Maxime LE GENTIL';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
         $this->bootstrap = true;
 
         parent::__construct();
 
-        $this->displayName = $this->l('Product List Module');
-        $this->description = $this->l('Expose an endpoint to get a list of products.');
+        $this->displayName = $this->l('Moteur de recherche de produits');
+        $this->description = $this->l('Permet de lister les produits à l\'aide d\'un moteur de recherche sur le front office.');
 
-        $this->confirmUninstall = $this->l('Are you sure you want to uninstall this module?');
+        $this->confirmUninstall = $this->l('Êtes-vous sûr de vouloir supprimer le module ?');
     }
 
     public function install()
